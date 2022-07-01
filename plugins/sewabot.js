7d36e82b58e1c6c00649b98310c6fb8d0a6e35d6
AhmadSFFBOT <108535438+AhmadSFFBOT@users.noreply.github.com>
2022-07-02 00:25:36 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -12,23 +12,33 @@ let handler = async (m, { conn }) => {
   title: `${await conn.getName(conn.user.jid)}`,
   description: ` *• SEWA BOT & UP TO PREMIUM •*
         
-1. Grup / 30 Hari
+1. Grup / Permanent
 Rp. 10.000 Dana
-Rp. 11.000 Pulsa
+Rp. 15.000 Pulsa
 Rp. 10.000 Gopay
+Rp. 10.000 Allpay
 
-2. Premium / 30 Hari
+2. Premium / Permanent
 Rp. 5.000 Dana
-Rp. 6.000 Pulsa
+Rp. 10.000 Pulsa
 Rp. 5.000 Gopay
+Rp. 5.000 Allpay
 
-3. Premium + Grup / 30 Hari
+3. Premium + Grup / Permanent
 Rp. 15.000 Dana
-Rp. 17.000 Pulsa
+Rp. 20.000 Pulsa
 Rp. 15.000 Gopay
+Rp. 15.000 Allpay
+
+Price 30 Hari(30 days)
+Grub / 30 Hari
+Rp. 5.000 Dana
+Rp. 10.000 Pulsa
+Rp. 5.000 Gopay
+Rp. 5.000 Allpay
 
 wa.me/${owner[0]}
-*Bukan Bot!!!*
+*INI NO BOT TPI INI JUGA OWNER*
 *Owner ${conn.user.name}*
 `,
   buttonText: 'Harga Sesuai Pasaran',
